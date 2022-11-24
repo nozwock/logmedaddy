085d74343aa13d1e0b48af07b69139f440e60605
nozwock <57829219+nozwock@users.noreply.github.com>
2022-11-25 02:46:30 +0530

    feat: add print --config-path option

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,22 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[command(group(
+            ArgGroup::new("info")
+                .args(["list", "config_path"])
+                .conflicts_with_all(["profile"]),
+        ))]
 pub struct Args {
     /// Use given profile for logs
-    #[arg(short, long, required = true, conflicts_with = "list")]
+    #[arg(short, long, required = true)]
     pub profile: Option<String>,
 
     /// Print available profiles
     #[arg(short, long)]
     pub list: bool,
+
+    /// Print config path
+    #[arg(long)]
+    pub config_path: bool,
 }
--- src/bin/logmedaddy.rs
+++ src/bin/logmedaddy.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::{fs::File, io::Write, process::exit};
 
-use logmedaddy::{log_profiles, Args};
+use logmedaddy::{defines, log_profiles, Args};
 
 fn main() -> anyhow::Result<()> {
     let cli = Args::parse();
@@ -16,7 +16,11 @@ fn main() -> anyhow::Result<()> {
                 .collect::<Vec<_>>()
                 .join("\n")
         );
-    }
+    };
+
+    if cli.config_path {
+        println!("{}", defines::app_config_path().to_string_lossy());
+    };
 
     if let Some(profile) = &cli.profile {
         let log_path = "./logmedaddy.log";
@@ -30,12 +34,12 @@ fn main() -> anyhow::Result<()> {
                 println!("No such profile exists!");
                 exit(1);
             }
-        }
+        };
     };
 
     // TODO: ability to pass multiple profiles
     // // TODO: obv impl the -p and -l options
-    // TODO: --config_path flag to print config path
+    // // TODO: --config_path flag to print config path
     // TODO: auto completion for shell?
 
     Ok(())
