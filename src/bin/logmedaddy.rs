e9b6b1d5880c9c023a6d57108b23ffbdb1632e2b
nozwock <57829219+nozwock@users.noreply.github.com>
2022-11-25 02:19:05 +0530

    refactor: remove unwrap calls & others

--- src/bin/logmedaddy.rs
+++ src/bin/logmedaddy.rs
@@ -1,44 +1,42 @@
 use clap::Parser;
-use logmedaddy::Args;
+use std::{fs::File, io::Write, process::exit};
 
-fn main() {
+use logmedaddy::{log_profiles, Args};
+
+fn main() -> anyhow::Result<()> {
     let cli = Args::parse();
     let cfg = logmedaddy::Config::load();
 
-    match &cli.list {
-        true => {
-            println!(
-                "Available profiles:\n{}",
-                cfg.profiles
-                    .iter()
-                    .map(|profile| profile.name.as_str())
-                    .collect::<Vec<_>>()
-                    .join("\n")
-            );
-        }
-        false => {}
-    };
-
-    match &cli.profile {
-        Some(profile) => {
-            use std::io::Write;
-            let log_path = "./logmedaddy.log";
-            let log = logmedaddy::log_profiles(vec![cfg
-                .profiles
+    if cli.list {
+        println!(
+            "Available profiles:\n{}",
+            cfg.profiles
                 .iter()
-                .find(|&x| &x.name == profile)
-                .unwrap()]);
-            std::fs::File::create(log_path)
-                .unwrap()
-                .write_all(log.as_bytes())
-                .unwrap();
-            println!("Log successfully saved at {:?}", log_path);
+                .map(|profile| profile.name.as_str())
+                .collect::<Vec<_>>()
+                .join("\n")
+        );
+    }
+
+    if let Some(profile) = &cli.profile {
+        let log_path = "./logmedaddy.log";
+        match cfg.profiles.iter().find(|&x| &x.name == profile) {
+            Some(profile) => {
+                let log = log_profiles(vec![profile]);
+                File::create(log_path)?.write_all(log.as_bytes())?;
+                println!("Log successfully saved at {:?}", log_path);
+            }
+            None => {
+                println!("No such profile exists!");
+                exit(1);
+            }
         }
-        None => {}
     };
 
     // TODO: ability to pass multiple profiles
-    // TODO: obv impl the -p and -l options
+    // // TODO: obv impl the -p and -l options
     // TODO: --config_path flag to print config path
     // TODO: auto completion for shell?
+
+    Ok(())
 }
